da0cec4e717828504635b91278eeb9f08f3ce09f
Yamil Llampi <yamil250100@gmail.com>
2022-12-26 21:25:46 -0500

    add files

--- OST_Tree.h
+++ OST_Tree.h
@@ -1,57 +1,73 @@
 #define OST_TREE
 #ifdef OST_TREE
+
 #include "OST_Tree_nodo.h"
-template <typename T>
-class OST_Tree{
+
+template<typename T>
+class OST_Tree {
 private:
     OST_Tree_nodo<T> *raiz;
     int maximo_puntos;
     int size_n;
 public:
     OST_Tree();
+
     OST_Tree(int, int);
-    void anadir(T*,T*,int);
-    void buscar(T*,int);
-    void eliminar(T*,int);
+
+    void anadir(T *, T *, int);
+
+    void buscar(T *, int);
+
+    void eliminar(T *, int);
+
     int size_nodo();
+
     void imprimir();
+
     ~OST_Tree();
 };
-template <typename T>
-OST_Tree<T>::OST_Tree(){
+
+template<typename T>
+OST_Tree<T>::OST_Tree() {
     maximo_puntos = 3;
-    raiz=new OST_Tree_nodo<T>();
+    raiz = new OST_Tree_nodo<T>();
     size_n = 3;
 }
-template <typename T>
-OST_Tree<T>::OST_Tree(int a, int m){
+
+template<typename T>
+OST_Tree<T>::OST_Tree(int a, int m) {
     maximo_puntos = m;
     size_n = a;
-    raiz=new OST_Tree_nodo<T>();
+    raiz = new OST_Tree_nodo<T>();
 }
-template <typename T>
-void OST_Tree<T>::anadir(T*nuevo_VBR,T*nuevo_MBR,int tiempo){
-    raiz->anadir(nuevo_VBR,nuevo_MBR,size_n,maximo_puntos,tiempo);
+
+template<typename T>
+void OST_Tree<T>::anadir(T *nuevo_VBR, T *nuevo_MBR, int tiempo) {
+    raiz->anadir(nuevo_VBR, nuevo_MBR, size_n, maximo_puntos, tiempo);
 }
-template <typename T>
-void OST_Tree<T>::buscar(T*region, int tiempo){
-    raiz->buscar(region,tiempo);
+
+template<typename T>
+void OST_Tree<T>::buscar(T *region, int tiempo) {
+    raiz->buscar(region, tiempo);
 }
-template <typename T>
-void OST_Tree<T>::eliminar(T*region, int tiempo){
-    raiz->eliminar(region,size_n,maximo_puntos,tiempo);
+
+template<typename T>
+void OST_Tree<T>::eliminar(T *region, int tiempo) {
+    raiz->eliminar(region, size_n, maximo_puntos, tiempo);
 }
-template <typename T>
-int OST_Tree<T>::size_nodo(){
+
+template<typename T>
+int OST_Tree<T>::size_nodo() {
     return size_n;
 }
-template <typename T>
-void OST_Tree<T>::imprimir(){
+
+template<typename T>
+void OST_Tree<T>::imprimir() {
     raiz->imprimir();
 }
-template <typename T>
-OST_Tree<T>::~OST_Tree()
-{
+
+template<typename T>
+OST_Tree<T>::~OST_Tree() {
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,45 +1,43 @@
 #include "OST_Tree.h"
-int main(){
+
+int main() {
     OST_Tree<int> alfa;
     int continuar;
     int tiempo;
-    int datos_MBR[4] ={2,4,3,4};
-    int datos_VBR[4] = {1,1,1,1};
-    alfa.anadir(datos_VBR,datos_MBR,0);
+    int datos_MBR[4] = {2, 4, 3, 4};
+    int datos_VBR[4] = {1, 1, 1, 1};
+    alfa.anadir(datos_VBR, datos_MBR, 0);
     alfa.imprimir();
-    cout<<"continuar?(0: no, 1: anadir, 2: buscar, 3: eliminar)"<<endl;
-    cin>>continuar;
-    while (continuar){
-        if(continuar == 1){
-            cout<<"datos VBR:"<<endl;
-            for (int i = 0; i < 4; i++){
-                cin>>datos_VBR[i];
+    cout << "continuar?(0: no, 1: anadir, 2: buscar, 3: eliminar)" << endl;
+    cin >> continuar;
+    while (continuar) {
+        if (continuar == 1) {
+            cout << "datos VBR:" << endl;
+            for (int i = 0; i < 4; i++) {
+                cin >> datos_VBR[i];
             }
-            cout<<"datos MBR:"<<endl;
-            for (int i = 0; i < 4; i++){
-                cin>>datos_MBR[i];
+            cout << "datos MBR:" << endl;
+            for (int i = 0; i < 4; i++) {
+                cin >> datos_MBR[i];
+            }
+        } else {
+            cout << "datos MBR:" << endl;
+            for (int i = 0; i < 4; i++) {
+                cin >> datos_MBR[i];
             }
         }
-        else{
-            cout<<"datos MBR:"<<endl;
-            for (int i = 0; i < 4; i++){
-                cin>>datos_MBR[i];
-            }
-        }
-        cout<<"tiempo:" <<endl;
+        cout << "tiempo:" << endl;
         cin >> tiempo;
-        if(continuar == 1){
-            alfa.anadir(datos_VBR,datos_MBR,tiempo);
-        }
-        else if(continuar == 2){
-            alfa.buscar(datos_MBR,tiempo);
-        }
-        else if(continuar == 3){
-            alfa.eliminar(datos_MBR,tiempo);
+        if (continuar == 1) {
+            alfa.anadir(datos_VBR, datos_MBR, tiempo);
+        } else if (continuar == 2) {
+            alfa.buscar(datos_MBR, tiempo);
+        } else if (continuar == 3) {
+            alfa.eliminar(datos_MBR, tiempo);
         }
         alfa.imprimir();
-        cout<<"continuar?(0: no, 1: anadir, 2: buscar, 3: eliminar)"<<endl;
-        cin>>continuar;
+        cout << "continuar?(0: no, 1: anadir, 2: buscar, 3: eliminar)" << endl;
+        cin >> continuar;
     }
     /**/
     return 0;
